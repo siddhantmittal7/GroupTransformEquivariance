ff8ee7b6e13577e1d76808ce77d78ca053300342
Siddhant Mittal <siddhantiitbmittal3@gmail.com>
2020-04-13 05:08:16 -0400

    Update visualization.py

--- visualization.py
+++ visualization.py
@@ -34,7 +34,7 @@ def visualize_transformation(x, model, device='cpu', figsize=(4, 3), cmap=None):
 
             print(type(module).__name__)
             param = [p.item() for p in param]
-            print('predicted parameter(s) =', param)
+            #print('predicted parameter(s) =', param)
 
             # transformer input (after coordinate transform)
             grid = module.coords(x.shape[-2:], ulim=module.ulim, vlim=module.vlim, device=x.device)
@@ -45,23 +45,23 @@ def visualize_transformation(x, model, device='cpu', figsize=(4, 3), cmap=None):
             grid = transform(coordinates.identity_grid(x.shape[-2:]).unsqueeze(0))
             x_out = F.grid_sample(x, grid)
 
-            if heatmap is None:
-                f, (ax1, ax2) = plt.subplots(1, 2, figsize=(figsize[0]*2 + 1, figsize[1]))
-            elif type(heatmap) is tuple:
-                # two parameters
-                f, (ax1, ax2, hm1, hm2) = plt.subplots(1, 4, figsize=(figsize[0]*4 + 3, figsize[1]))
-                hm1.plot(heatmap[0].squeeze().cpu().numpy())
-                hm2.plot(heatmap[1].squeeze().cpu().numpy())
-                hm1.set_title('transformer feature map 1')
-                hm2.set_title('transformer feature map 2')
-                hm1.grid(True)
-                hm2.grid(True)
-            else:
-                # one parameter
-                f, (ax1, ax2, hm1) = plt.subplots(1, 3, figsize=(figsize[0]*3 + 2, figsize[1]))
-                hm1.plot(heatmap.squeeze().cpu().numpy())
-                hm1.set_title('transformer feature map')
-                hm1.grid(True)
+#             if heatmap is None:
+#                 f, (ax1, ax2) = plt.subplots(1, 2, figsize=(figsize[0]*2 + 1, figsize[1]))
+#             elif type(heatmap) is tuple:
+#                 # two parameters
+#                 f, (ax1, ax2, hm1, hm2) = plt.subplots(1, 4, figsize=(figsize[0]*4 + 3, figsize[1]))
+#                 hm1.plot(heatmap[0].squeeze().cpu().numpy())
+#                 hm2.plot(heatmap[1].squeeze().cpu().numpy())
+#                 hm1.set_title('transformer feature map 1')
+#                 hm2.set_title('transformer feature map 2')
+#                 hm1.grid(True)
+#                 hm2.grid(True)
+#             else:
+#                 # one parameter
+#                 f, (ax1, ax2, hm1) = plt.subplots(1, 3, figsize=(figsize[0]*3 + 2, figsize[1]))
+#                 hm1.plot(heatmap.squeeze().cpu().numpy())
+#                 hm1.set_title('transformer feature map')
+#                 hm1.grid(True)
                 
             ax1.imshow(tensor_to_numpy(x_in), cmap=cmap)
             ax2.imshow(tensor_to_numpy(x_out), cmap=cmap)
@@ -70,11 +70,11 @@ def visualize_transformation(x, model, device='cpu', figsize=(4, 3), cmap=None):
             prev_transform = transform
             plt.show()
             
-        grid = model.model.coords(x.shape[-2:], device=x.device)
-        grid = prev_transform(grid.unsqueeze(0))
-        x_in = F.grid_sample(x, grid)
-        f = plt.figure(figsize=figsize)
-        plt.imshow(tensor_to_numpy(x_in), cmap=cmap)
-        plt.title('output after final coordinate transformation')
-        plt.show()
+#         grid = model.model.coords(x.shape[-2:], device=x.device)
+#         grid = prev_transform(grid.unsqueeze(0))
+#         x_in = F.grid_sample(x, grid)
+#         f = plt.figure(figsize=figsize)
+#         plt.imshow(tensor_to_numpy(x_in), cmap=cmap)
+#         plt.title('output after final coordinate transformation')
+#         plt.show()
         
